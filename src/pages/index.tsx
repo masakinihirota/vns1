06d66073e1d6dab1f8f071eda9aff6e8fd765f46
masakinihirota <masakinihirota@gmail.com>
2022-09-06 18:08:37 +0900

    add srcディレクトリ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { Layout } from '@/components/Layout'
+import { Layout } from '../components/Layout'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
