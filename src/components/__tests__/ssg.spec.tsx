9a399f08c16a033c95f28dfc0ed431cca83533e8
masakinihirota <masakinihirota@gmail.com>
2022-09-09 09:39:01 +0900

    storyファイルの整理

---
+++ src/components/__tests__/ssg.spec.tsx
@@ -0,0 +1,11 @@
+import Ssg from '../../pages/ssg'
+import { render } from '@testing-library/react'
+
+// コンポーネント以外での場所のテスト 表示した文字列のみ
+describe('ssg.tsxコンポーネント', () => {
+  test('should first', () => {
+    // SSGのテストの書き方がわからない
+    // const { getByText } = render(<Ssg />)
+    // expect(getByText('龍と苺')).toBeTruthy()
+  })
+})
--- src/pages/__view_button.tsx
+++ src/pages/__view_button.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/Button/Button"
+import { Button } from "@/components_test/Button/Button"
 // import { Button } from "../components/Button/Button"
 
 import type { NextPage } from "next"
